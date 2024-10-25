a68fcb2e379c905bdca83ccad4f65aeabdeb0d53
Bebel19 <belgnaoui.othman@gmail.com>
2024-10-25 15:37:12 +0200

    Modified prints formating

--- src/test_xy_ot.py
+++ src/test_xy_ot.py
@@ -6,18 +6,18 @@ import numpy as np
 
 # Afficher chaque transformation pour suivre le calcul
 for i in range(len(dh['sigma_i'])):
-    print(f"Transformation T({i},{i+1}):")
+    print(f"Transformation T({i},{i+1}):\n")
     t_i_ip1 = matrice_Tim1_Ti(dh["sigma_i"][i], dh["a_i_m1"][i], dh["alpha_i_m1"][i], dh["r_i"][i], round_m=round_p)
-    print(t_i_ip1)
-    print("\n")
+    print(f"{t_i_ip1}\n")
+
 
 # Calcul de la transformation complète T(0,3)
-print("Transformation T(0,3):")
+print(f"Transformation T(0,{len(dh['sigma_i'])}):\n")
 matrice_T0Tn = matrice_Tn(dh, round_m=round_p)
-print(matrice_T0Tn)
+print(f"{matrice_T0Tn}\n")
 
 # Extraction des coordonnées (x, y, z) de la transformation finale
 xyz = xy_Ot(matrice_T0Tn)
-print("\nCoordonnées finales (x, y, z):")
-print(xyz)
-print(H(xyz,Xd,rayon_max_p=rayon_max1_5))
+print("Coordonnées finales (x, y, z):\n")
+print(f"{xyz}\n")
+print(f"{H(xyz,Xd,rayon_max_p=rayon_max1_5)}\n")
