a42af239566844d610f514a6245782c7bcd379c6
Ismael Monge Rouchdi <01ismaelmon@gmail.com>
2025-01-07 20:56:49 +0100

    Il manqui initialisation du robot au ptn A dans l'animation

--- Tests/main.py
+++ Tests/main.py
@@ -20,11 +20,11 @@ if __name__ == "__main__":
         if choix == "2" :
             V1 = float(input("Vitesse 1 :\n"))
             V2 = float(input("Vitesse 2 :\n"))
-            A = np.array([500, float(input("Coordonnée y pour A :\n")), float(input("Coordonnée z pour A :\n"))])
-            B = np.array([500, float(input("Coordonnée y pour B :\n")), float(input("Coordonnée z pour B :\n"))])
+            x=float(input("Coordonnée x partagé par A et B :\n"))
+            A = np.array([x, float(input("Coordonnée y pour A :\n")), float(input("Coordonnée z pour A :\n"))])
+            B = np.array([x, float(input("Coordonnée y pour B :\n")), float(input("Coordonnée z pour B :\n"))])
             traj(A,B,V1,V2,Debug=True)
             print("Voulez vous lancer une simulation avec ces données?")
             k = input("1: Oui 2: Non\n")
             if k == "1" :
                 bras_rob_model3D_animation(A,B,V1,V2)
-        
--- src/main_analyse.py
+++ src/main_analyse.py
@@ -132,19 +132,5 @@ def main_analyse():
         print("\n")
     else:
         pass
-    # Genération de trajectoire
-    # Test génération de trajectoire
-
-    print("Pour un cercle avec les points A et B alignés sur l'axe Y veuillez introduire")
-    zA = int(input("Coordonnée z du point A:"))
-    zB = int(input("Coordonnée z du point B:"))
-    A = np.array([500, 100, zA])
-    B = np.array([500, 120, zB])
-
-    result_a, message_a = est_point_atteignable(A)
-    result_b, message_b = est_point_atteignable(B)
-
-    print(f"Point A: {message_a}",f"\nOn arrive aux coordonnées:{result_a}")
-    print(f"Point B: {message_b}",f"\nOn arrive aux coordonnées:{result_b}")
-
+    
     main_analyse()
